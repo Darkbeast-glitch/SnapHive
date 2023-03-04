319e837beb6e20a808521101a58eee52f9ef8433
Julius <bbjuliu4@gmail.com>
2023-03-04 18:11:37 +0100

    updated everything on the frontend

--- snaphiveApp/models.py
+++ snaphiveApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    fullname = models.CharField(max_length=200, blank=False, null=False)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    category = models.CharField(max_length=200, blank=True, null=True)
+    others = models.CharField(max_length=200, blank=True, null=True)
+    subject = models.CharField(max_length=200, blank=False, null=False)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+
+class newsLetter(models.Model):
+    email_letter = models.EmailField()
+
+    def __str__(self):
+        return self.email_letter
+
+    class Meta:
+        verbose_name = "NewsLetter"
+        verbose_name_plural = "NewsLetters"
--- snaphiveApp/views.py
+++ snaphiveApp/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
+from .models import Contact, newsLetter
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def HomePage(request):
-    context = {}
+
+    contact = Contact.objects.all()
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        category = request.POST["category"]
+        others = request.POST["otherInput"]
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+
+        contact_form = Contact(fullname=name, email=email, phone=phone, category=category, others=others,
+                               subject=subject, message=message)
+        contact_form.save()
+
+        messages.success(
+            request, "Your message has been received, SnapHive rep will get to you in less than 24hrs.")
+        contact_form.save()
+
+        # # letteremail = newsLetter.objects.all()
+        # if request.method == 'POST':
+        #     email_letter = request.POST['newsletter_email']
+
+        #     email_form = newsLetter(email=email_letter)
+        #     email_form.save()
+
+        #     messages.success(
+        #         request, "Your message has been received, SnapHive rep will get to you in less than 24hrs.")
+        # contact_form.save()
+
+    context = {
+        'contact': contact,
+
+    }
 
     return render(request, 'index.html', context)
